55d49017cc630211b248fd461edb779c4636e6be
eponkratsyev <ponk@tut.by>
2025-05-23 17:41:55 +0300

    Add component

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <>
+            <img src="/logo.png" alt="Icon"/>
+            <h1>Welcome to this NextJS course</h1>
+        </>
+    )
+}
