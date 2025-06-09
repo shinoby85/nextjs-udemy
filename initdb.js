2d0cf978d82156c008c2d2b1b93952aa026ae696
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-06-09 17:41:20 +0300

    Create sqLite DB

--- initdb.js
+++ initdb.js
@@ -165,56 +165,30 @@ const dummyMeals = [
 ];
 
 db.prepare(`
-    CREATE TABLE IF NOT EXISTS meals
-    (
-        id
-        INTEGER
-        PRIMARY
-        KEY
-        AUTOINCREMENT,
-        slug
-        TEXT
-        NOT
-        NULL
-        UNIQUE,
-        title
-        TEXT
-        NOT
-        NULL,
-        image
-        TEXT
-        NOT
-        NULL,
-        summary
-        TEXT
-        NOT
-        NULL,
-        instructions
-        TEXT
-        NOT
-        NULL,
-        creator
-        TEXT
-        NOT
-        NULL,
-        creator_email
-        TEXT
-        NOT
-        NULL
+    CREATE TABLE IF NOT EXISTS meals (
+                                         id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                         slug TEXT NOT NULL UNIQUE,
+                                         title TEXT NOT NULL,
+                                         image TEXT NOT NULL,
+                                         summary TEXT NOT NULL,
+                                         instructions TEXT NOT NULL,
+                                         creator TEXT NOT NULL,
+                                         creator_email TEXT NOT NULL
     )
 `).run();
 
 async function initData() {
   const stmt = db.prepare(`
-      INSERT INTO meals
-      VALUES (null,
-              @slug,
-              @title,
-              @image,
-              @summary,
-              @instructions,
-              @creator,
-              @creator_email)
+      INSERT INTO meals VALUES (
+                                   null,
+                                   @slug,
+                                   @title,
+                                   @image,
+                                   @summary,
+                                   @instructions,
+                                   @creator,
+                                   @creator_email
+                               )
   `);
 
   for (const meal of dummyMeals) {
